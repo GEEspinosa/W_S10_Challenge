f64fcd20f0492c5ecde86540a18cdd3e26efbc79
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-05-28 18:37:43 -0700

    wip

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,26 +1,31 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-//import {useGetOrdersQuery} from '../state/OrdersApi'
+//import { useSelector } from 'react-redux'
+import {useGetOrdersQuery} from '../state/OrdersApi'
 
 export default function OrderList() {
-  //const {data: orders} = useGetOrdersQuery()
+  const { data: orders} = useGetOrdersQuery()
+
   
   
   
-  const orders = useSelector(st => st.orderList.orders)
+  //const orders = useSelector(st => st.orderList.orders)
   
+  //const orders = [{id: 1, customer: 'Cher', size: 'M', toppings: []}]
+
+
   return (
     <div id="orderList">
       <h2>Pizza Orders</h2>
       <ol>
         
       {
-        // console.log(orders)
-          orders.map(() => {
+        
+        
+          orders.map((order, key) => {
             return (
-              <li key={1}>
+              <li key={key}>
                 <div>
-                  order details here
+                  {`${order.customer} ordered a size ${order.size} with ${ order.toppings.length ? order.toppings.length : 'no'} ${order.toppings.length > 1 || order.toppings.length === 0 ? 'toppings' : 'topping'}`}
                 </div>
               </li>
             )
--- frontend/state/OrdersApi.js
+++ frontend/state/OrdersApi.js
@@ -3,10 +3,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const ordersApi = createApi ({
     reducerPath: 'ordersApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/pizza/'}),
+    tagTypes: ['Orders'],
     endpoints: build => ({
 
         getOrders: build.query({
-            query: () => 'history'
+            query: () => 'history',
+            providesTags: ['Orders'],
         }),
 
         createOrder: build.mutation({
@@ -14,7 +16,8 @@ export const ordersApi = createApi ({
                 url: 'order',
                 method: 'POST',
                 body: order
-            })
+            }),
+            invalidatesTages: ['Orders']
         })
     })
 })
